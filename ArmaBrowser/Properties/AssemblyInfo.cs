a3e1a151b3a70e55ca219906796381a498c301c3
sonabit <sonabit@gmx.de>
2015-04-08 21:58:45 +0200

    Version 0.7.1.77 Fix UI-Blocking

--- ArmaBrowser/Properties/AssemblyInfo.cs
+++ ArmaBrowser/Properties/AssemblyInfo.cs
@@ -51,8 +51,8 @@ using System.Windows;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.7.0.76")]
-[assembly: AssemblyFileVersion("0.7.0.76")]
+[assembly: AssemblyVersion("0.7.1.77")]
+[assembly: AssemblyFileVersion("0.7.1.77")]
 
 
 //[assembly: InternalsVisibleTo("UnitTestProject1")]
