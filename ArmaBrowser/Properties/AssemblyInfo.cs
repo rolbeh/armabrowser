3318c244402e72bc0827b64246ffce2a6f62a85b
sonabit <sonabit@gmx.de>
2017-06-10 20:03:58 +0200

    remove public key of trusted Internals assemblies

--- ArmaBrowser/Properties/AssemblyInfo.cs
+++ ArmaBrowser/Properties/AssemblyInfo.cs
@@ -23,11 +23,13 @@ using System.Windows;
 [assembly: AssemblyVersion("2016.4.2.104")]
 [assembly: AssemblyFileVersion("2016.4.2.104")]
 
-[assembly: InternalsVisibleTo("IntegrationsTest.ArmaBrowser, PublicKey=002400000480000014010000060200000024000052534131000800000100010085646f1a8d55e0" +
-                            "0148bd1a32751795ddf3f1b9b18ee9da937bda8cdeded17c16412450552bed589b74285a0b027f" +
-                            "b4249b7500d83d181d7b12026a7c8c72492fca513e402313cfb671d0a8de8f334e6d2617b784b9" +
-                            "659265056ef31fa007e85ed23c03c80a2b8e3baa6d23319c662358c2e60a707cd2b84e9d823381" +
-                            "4d2d433828d68af06997ccb165f710b1c09e3ac2e9dff46e0ed9a351b6117db213130db21b621e" +
-                            "d41881d4507d0474bac7bc5eb10cb22d337a905f6524c1a6f20e7144fc5b458411c26c2372586b" +
-                            "aa2b8b64df62864a3c4624009d31254fc9233434c7ef5de04fe44a830740a2d011dee34a5eae51" +
-                            "f2ec4added16a7a5e5e58a10f0dbfd")]
+//[assembly: InternalsVisibleTo("IntegrationsTest.ArmaBrowser, PublicKey=002400000480000014010000060200000024000052534131000800000100010085646f1a8d55e0" +
+//                            "0148bd1a32751795ddf3f1b9b18ee9da937bda8cdeded17c16412450552bed589b74285a0b027f" +
+//                            "b4249b7500d83d181d7b12026a7c8c72492fca513e402313cfb671d0a8de8f334e6d2617b784b9" +
+//                            "659265056ef31fa007e85ed23c03c80a2b8e3baa6d23319c662358c2e60a707cd2b84e9d823381" +
+//                            "4d2d433828d68af06997ccb165f710b1c09e3ac2e9dff46e0ed9a351b6117db213130db21b621e" +
+//                            "d41881d4507d0474bac7bc5eb10cb22d337a905f6524c1a6f20e7144fc5b458411c26c2372586b" +
+//                            "aa2b8b64df62864a3c4624009d31254fc9233434c7ef5de04fe44a830740a2d011dee34a5eae51" +
+//                            "f2ec4added16a7a5e5e58a10f0dbfd")]
+
+[assembly: InternalsVisibleTo("IntegrationsTest.ArmaBrowser")]
