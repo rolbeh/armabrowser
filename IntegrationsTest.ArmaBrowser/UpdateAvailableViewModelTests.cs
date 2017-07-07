716741008167d31448a02667d72fdb839cef48b0
sonabit <sonabit@gmx.de>
2017-07-07 21:20:25 +0200

    TDD for update   viewmodel

---
+++ IntegrationsTest.ArmaBrowser/UpdateAvailableViewModelTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ArmaBrowser.ViewModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IntegrationsTest.ArmaBrowser
+{
+
+    [TestClass]
+    public class UpdateAvailableViewModelTests
+    {
+        [TestMethod, TestCategory("onlylocal")]
+        public async Task CheckForUdpateTest()
+        {
+            // given
+            UpdateAvailableViewModel viewModel = new UpdateAvailableViewModel();
+
+            // when
+            await viewModel.CheckForUpdates();
+        }
+    }
+}
